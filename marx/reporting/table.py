361c4dcdeb9b0f89ce1acc8fc1de940a4582232c
angmorpri <angelmorenoprieto@gmail.com>
2024-02-29 15:58:44 +0100

    Sistema TableBuilder construido y probado

--- marx/reporting/table.py
+++ marx/reporting/table.py
@@ -1,10 +1,11 @@
 # Python 3.10.11
 # Creado: 28/02/2024
-"""Módulo que define la clase "TableBuilder", que permite crear tablas
-jerárquicas de datos que luego pueden ser exportadas a distintos formatos.
+"""Módulo que define la clase 'TableBuilder', que permite crear tablas de datos
+jerárquicas que luego pueden ser exportadas a distintos formatos.
 
-La clase principal es TableBuilder, que se compone de diferentes TableNode que
-representan las filas de la tabla.
+La clase principal es 'TableBuilder', que gestiona la creación de la tabla y
+mantiene las 'TableRow' que la componen. Ambas clases heredan de 'Node', para
+permitir la jerarquización de las filas de la tabla.
 
 """
 from __future__ import annotations
@@ -14,72 +15,67 @@ from types import MappingProxyType
 from typing import Any, Iterable, Literal
 
 
-class TableNode:
-    """Clase que representa un nodo de una tabla de datos.
+class _GUARD_CLASS:
+    pass
 
-    Un nodo es una fila de la tabla que también se comporta como un nodo dentro
-    de una estructura de árbol, permitiendo generar una tabla jerarquizada.
 
-    Como parte de un nodo de árbol, se compone de un TableBuilder maestro
-    ('master'), de un padre ('parent') y de una lista de hijos ('children').
-    Como fila, contiene un título ('title') y una lista de valores ('values').
-    En función del tipo de fila, los valores pueden ser directamente datos, o
-    agregaciones de datos de otras filas. Las opciones son:
-        - "VALUE": los valores son datos directos.
-        - "SUM_CHILDREN": los valores son la suma de los valores de sus nodos
-            hijos.
-        - "SUM_SIBLINGS": los valores son la suma de los valores de sus nodos
-            hermanos.
+_GUARD = _GUARD_CLASS()
 
-    Además, cada nodo tiene un identificador único ('id'), que no puede
-    repetirse dentro de un mismo TableBuilder. También se dispone de una llave
-    'order' para determinar el orden en el que tienen que aparecer los hijos
-    cuando sean representados.
 
-    El constructor recibe los siguientes parámetros:
-        - parent: el nodo padre de la fila. Debe definir 'master'. Obligatorio.
-        - title: el título de la fila. Obligatorio.
-        - id: el identificador único de la fila. Si no se proporciona, se
-            utilizará 'title'.
-        - values: indica el tipo de valor que gestionará la fila. Debe ser
-            "VALUE", "SUM_CHILDREN" o "SUM_SIBLINGS". Por defecto, será
-            "VALUE".
-        - order: el orden en el que aparecerá la fila. Si no se indica, se
-            utilizará el orden de creación.
+class Node:
+    """Clase que representa un nodo de una estructura de árbol.
+
+    Proporciona los atributos típicos 'parent' y 'children', y métodos para
+    crear y gestionar los nodos hijos.
+
+    En concreto, los atributos son:
+        - id: el identificador único del nodo. No puede haber dos nodos con el
+            mismo ID en la misma estructura.
+        - parent: el nodo padre del nodo actual.
+        - master: el nodo raíz de la estructura.
+        - children: una lista de nodos hijos del nodo actual.
+        - siblings: una lista de nodos hermanos del nodo actual, sin incluir
+            al nodo actual.
+        - order_key: la clave para ordenar los nodos hijos. Si no se indica,
+            se utilizará el orden de creación.
+
+    Y los métodos disponibles son:
+        - append: añade un nodo hijo al nodo actual.
+        - get: devuelve un nodo hijo con el ID indicado. También permite
+            devolver cualquier nodo subordinado al nodo actual. Este último
+            comportamiento se obtiene también accediendo en modo lista al nodo.
+        - has_children: indica si el nodo tiene hijos o no.
+        - has_grandchildren: indica si el nodo tiene nietos o no.
+        - __iter__: devuelve un iterador sobre los nodos hijos del nodo,
+            ordenados.
+
+    El constructor requiere, al menos, el nodo padre del nodo actual y el ID
+    del nuevo nodo. Si se quiere indicar que el nodo es raíz de una nueva
+    estructura, se debe indicar explícitamente con 'None'. Opcionalmente, se
+    puede indicar la clave de ordenación de los nodos hijos mediante
+    'order_key'.
 
     """
 
-    GLOBAL_INDEX = 100
+    ORDER_INDEX = 100
 
-    def __init__(
-        self,
-        parent: TableNode | TableBuilder,
-        title: str,
-        id: str | None = None,
-        *,
-        values: Literal["VALUE", "SUM_CHILDREN", "SUM_SIBLINGS"] = "VALUE",
-        order: Any = None,
-    ):
-        # Nodo
-        if not parent.master:
-            raise ValueError("El nodo padre debe definir un TableBuilder maestro.")
+    def __init__(self, parent: Node | None, id: Any, *, order_key: Any = None):
+        self.id = id
         self.parent = parent
-        self.master = parent.master
         self.children = []
-
-        # Fila
-        self.title = title
-        self.id = id or title
-        self.values_type = values
-        self._values = {key: 0.0 for key in self.master.headers}
-        if not order:
-            self.order = TableNode.GLOBAL_INDEX
-            TableNode.GLOBAL_INDEX += 1
+        if self.parent is None:
+            self.master = self
         else:
-            self.order = order
+            self.master = parent.master
+            self.parent.children.append(self)
+        if order_key:
+            self.order_key = order_key
+        else:
+            self.order_key = Node.ORDER_INDEX
+            Node.ORDER_INDEX += 1
 
     @property
-    def siblings(self) -> Iterable[TableNode]:
+    def siblings(self) -> Iterable[Node]:
         """Devuelve la lista de nodos hermanos del nodo actual, SIN contar el
         nodo actual.
 
@@ -88,7 +84,7 @@ class TableNode:
 
     @property
     def generation(self) -> int:
-        """Devuelve la generación del nodo en la jerarquía de la tabla.
+        """Devuelve la generación del nodo en la jerarquía del árbol.
 
         La generación de un nodo raíz es 0, y la de sus hijos es 1, y así
         sucesivamente.
@@ -100,6 +96,117 @@ class TableNode:
             generation += 1
             parent = parent.parent
         return generation
+
+    def has_children(self) -> bool:
+        """Indica si el nodo tiene hijos o no."""
+        return bool(self.children)
+
+    def has_grandchildren(self) -> bool:
+        """Indica si el nodo tiene nietos o no."""
+        return any(child.has_children() for child in self.children)
+
+    # Creación y gestión de nodos hijos
+    def append(self, id: Any, *args: Any, **kwargs: Any) -> None:
+        """Añade un nodo hijo al nodo actual, y lo devuelve.
+
+        El ID del nuevo nodo no debe existir en la estructura actual. Si lo
+        hace, este método no hará nada y devolverá el nodo ya existente.
+
+        """
+        if id in self:
+            return self[id]
+        return self.__class__(self, id, *args, **kwargs)
+
+    def get(self, id: Any, fallback: Any = _GUARD, *, only_children: bool = True) -> Node:
+        """Devuelve un nodo hijo con el ID indicado.
+
+        Se puede proporcionar un valor por defecto para devolver en caso de no
+        encontrar el nodo. Si no se proporciona y no se encuentra el nodo, se
+        lanzará un KeyError.
+
+        Si se quiere buscar cualquier nodo subordinado al nodo actual, se debe
+        indicar 'only_children' como False.
+
+        """
+        if only_children:
+            for child in self.children:
+                if child.id == id:
+                    return child
+            if fallback is _GUARD:
+                raise KeyError(f"No se ha encontrado ningún nodo con el ID '{id}'.")
+            return fallback
+        else:
+            found = None
+            for child in self.children:
+                if child.id == id:
+                    return child
+                elif child.has_children():
+                    found = child.get(id, fallback=None, only_children=False)
+                    if found:
+                        return found
+            if fallback is _GUARD:
+                raise KeyError(f"No se ha encontrado ningún nodo con el ID '{id}'.")
+            return fallback
+
+    def __getitem__(self, id: Any) -> Node:
+        """Sinónimo para 'get(id, only_children=False)'."""
+        return self.get(id, only_children=False)
+
+    def __contains__(self, id: Any) -> bool:
+        """Indica si el nodo actual contiene un nodo con el ID indicado."""
+        try:
+            self.get(id, only_children=False)
+        except KeyError:
+            return False
+        return True
+
+    def __iter__(self) -> Iterable[Node]:
+        """Devuelve un iterador sobre los nodos hijos del nodo, ordenados."""
+        return iter(sorted(self.children, key=lambda node: node.order_key))
+
+    def __str__(self) -> str:
+        return f"Node({self.generation}, {self.id})"
+
+
+# OLD #
+class TableRow(Node):
+    """Clase que representa una fila de una tabla de datos.
+
+    Se compone de un título ('title') y de un diccionario de valores ('values')
+    que, en función del tipo de valor que almacene la fila ('values_type'),
+    pueden ser datos directos, o agregaciones de datos de otras filas. Las
+    opciones son:
+
+        - "VALUE": los valores son datos directos.
+        - "SUM_CHILDREN": los valores son la suma de los valores de sus nodos
+            hijos.
+        - "SUM_SIBLINGS": los valores son la suma de los valores de sus nodos
+            hermanos.
+
+    Además, hereda de 'Node' para permitir la jerarquización de las filas de la
+    tabla.
+
+    El constructor recibe en orden, el nodo padre de la fila, el ID de la fila,
+    el título de la fila, y opcionalmente, el tipo de valores que gestionará la
+    fila.
+
+    Si no se proporciona un título para la fila, se utilizará el ID como tal.
+
+    """
+
+    def __init__(
+        self,
+        parent: Node,
+        id: str,
+        title: str | None = None,
+        *,
+        values: Literal["VALUE", "SUM_CHILDREN", "SUM_SIBLINGS"] = "VALUE",
+        order_key: Any = None,
+    ):
+        super().__init__(parent, id, order_key=order_key)
+        self.title = title or id
+        self.values_type = values
+        self._values = {key: 0 for key in parent.master.headers}
 
     @property
     def values(self) -> dict[str, float] | MappingProxyType[str, float]:
@@ -114,120 +221,70 @@ class TableNode:
             return self._values
         elif self.values_type == "SUM_CHILDREN":
             for key in self._values:
-                self._values[key] = sum(child._values[key] for child in self.children)
+                self._values[key] = round(sum(child.values[key] for child in self.children), 2)
         elif self.values_type == "SUM_SIBLINGS":
             for key in self._values:
-                self._values[key] = sum(sibling._values[key] for sibling in self.siblings)
+                self._values[key] = round(sum(sibling.values[key] for sibling in self.siblings), 2)
         return MappingProxyType(self._values)
 
-    def has_children(self) -> bool:
-        """Indica si el nodo tiene hijos o no."""
-        return bool(self.children)
-
-    def has_grandchildren(self) -> bool:
-        """Indica si el nodo tiene nietos o no."""
-        return any(child.has_children() for child in self.children)
-
-    def append(
-        self, title: str, id: str | None = None, *, values: str = "VALUE", order: Any = None
-    ) -> TableNode:
-        """Añade un nodo hijo a la fila actual.
-
-        También se añade al diccionario global de nodos del TableBuilder
-        maestro.
-
-        Si se trata de añadir un nodo cuyo ID ya exista, no creará ni añadirá
-        un nuevo nodo y devolverá el ya existente.
-
-        """
-        id = id or title
-        if id in self.master.nodes:
-            return self.master.nodes[id]
-
-        child = TableNode(self, title, id, values=values, order=order)
-        self.children.append(child)
-        self.master.nodes[id] = child
-        return child
-
-    def __getitem__(self, id: str) -> TableNode:
-        """Devuelve un nodo con el ID indicado que esté subordinado a éste.
-
-        Si no lo encuentra, lanzará un KeyError.
-
-        """
-        for child in self.children:
-            if child.id == id:
-                return child
-            elif child.has_children():
-                return child[id]
-        raise KeyError(
-            f"No se ha encontrado ningún nodo con el ID '{id}'." f" desde el nodo '{self.id}'"
-        )
-
-    def __iter__(self) -> Iterable[TableNode]:
-        """Devuelve un iterador sobre los nodos hijos de la fila, ordenados."""
-        return iter(sorted(self.children, key=lambda node: node.order))
-
     def __str__(self) -> str:
-        return f"[{self.id:*>10}]{'  '*self.generation}{self.title} | {self.values}"
+        id = str(self.id)[:10]
+        title = f"{'  ' * self.generation}{self.title}"
+        return f"[{id:*>10}] {title: <40} | {self.values}"
 
 
-class TableBuilder:
+class TableBuilder(Node):
     """Clase que gestiona la creación de una tabla de datos jerárquica.
 
-    Esta clase actúa como un concentrador de los nodos de la tabla. A su vez,
-    define los encabezados de las columnas.
+    Esta clase actúa como nodo maestro de la estructura, y mantiene una lista
+    de nodos hijos que representan las filas de la tabla. Además, define las
+    cabeceras de las columnas de la tabla.
 
-    Se proporciona el método 'append' para añadir nodos a la tabla. También se
-    puede acceder a los nodos directamente en modo lista (table[...]) usando el
-    ID de cualquier nodo.
-
-    El constructor recibe un iterable con los títulos de las cabeceras de las
-    columnas de la tabla.
+    El constructor recibe un iterable con las cabeceras de las columnas de la
+    tabla. Opcionalmente, se puede dar también un título para la tabla en
+    conjunto.
 
     """
 
-    def __init__(self, headers: Iterable[str]) -> None:
+    def __init__(self, headers: Iterable[str], *, title: str = "Tabla") -> None:
+        super().__init__(None, "MASTER")
+        self.title = title
         self.headers = list(headers)
-        self.nodes = {}
-        self.master = self
-        self.parent = None
-        self.children = []
 
-    def append(
-        self, title: str, id: str | None = None, *, values: str = "VALUE", order: Any = None
-    ) -> TableNode:
-        """Añade un nodo a la tabla.
+    def append(self, id: str, *args, **kwargs) -> TableRow:
+        """Añade una fila a la tabla."""
+        if id in self:
+            return self[id]
+        return TableRow(self, id, *args, **kwargs)
 
-        Si se trata de añadir un nodo cuyo ID ya exista, no creará ni añadirá
-        un nuevo nodo y devolverá el ya existente.
-
-        """
-        id = id or title
-        if id in self.nodes:
-            return self.nodes[id]
-
-        node = TableNode(self, title, id, values=values, order=order)
-        self.nodes[id] = node
-        self.children.append(node)
-        return node
-
-    def __getitem__(self, id: str) -> TableNode:
-        """Devuelve un nodo con el ID indicado que esté subordinado a la tabla.
-
-        Si no lo encuentra, lanzará un KeyError.
-
-        """
-        if id in self.nodes:
-            return self.nodes[id]
-        raise KeyError(f"No se ha encontrado ningún nodo con el ID '{id}'.")
-
-    def __iter__(self) -> Iterable[TableNode]:
-        """Devuelve un iterador sobre los nodos de la tabla, ordenados."""
-        return iter(sorted(self.children, key=lambda node: node.order))
+    def _ln(self, node: Node, output: list[str]) -> list[str]:
+        output.append(str(node))
+        for child in node:
+            output = self._ln(child, output)
+        return output
 
     def __str__(self) -> str:
-        return "\n".join(str(node) for node in self)
+        s = [f"Tabla: {self.title}"]
+        for child in self:
+            s = self._ln(child, s)
+        return "\n".join(s)
+
+
+if __name__ == "__xmain__":
+    table = TableBuilder(["A", "B", "C"])
+    table.append("Frutas", values="SUM_CHILDREN")
+    f = table["Frutas"].append("Manzana")
+    table["Frutas"].append("Pera")
+    f.values["A"] = 10
+    f.values["B"] = 20
+    f.values["C"] = 30
+    table["Frutas"]["Pera"].values["A"] = 5
+    table["Frutas"]["Pera"].values["B"] = 50
+    table["Frutas"]["Pera"].values["C"] = 500
+    table.append("Verduras", values="SUM_CHILDREN")
+    f = table["Frutas"].append("Manzana")
+    f.values["B"] += 1000
+    print(table)
 
 
 if __name__ == "__main__":
@@ -239,13 +296,13 @@ if __name__ == "__main__":
     for _ in range(100):
         acc = random.choice(["Ingresos", "Básicos", "Personales", "Hucha", "Reserva", "Inversión"])
         random.shuffle(flows)
-        amount = random.uniform(1, 3000)
+        amount = round(random.uniform(1, 3000), 2)
         events.append({"account": acc, flows[0]: acc, flows[1]: "Contraparte", "amount": amount})
 
     table = TableBuilder(["A", "B", "C"])
     table.append("Activos", values="SUM_CHILDREN")
-    table["Activos"].append("Corrientes", "COR", values="SUM_CHILDREN")
-    table["Activos"].append("Financieros", "FIN", values="SUM_CHILDREN")
+    table["Activos"].append("COR", "Corrientes", values="SUM_CHILDREN")
+    table["Activos"].append("FIN", "Financieros", values="SUM_CHILDREN")
     table.append("Pasivos", values="SUM_CHILDREN")
     for event in events:
         account = event["account"]
@@ -266,7 +323,7 @@ if __name__ == "__main__":
             else:
                 category = "Caja"
                 order = 1
-            table["COR"].append(category, values="SUM_CHILDREN", order=order)
+            table["COR"].append(category, values="SUM_CHILDREN", order_key=order)
             target = table["COR"][category].append(account, values="VALUE")
         for key in ("A", "B", "C"):
             if key == "A":
@@ -276,5 +333,4 @@ if __name__ == "__main__":
             else:
                 target.values[key] += amount * 1.25
 
-        print(table)
-        print("\n--------------------------------------------------\n")
+    print(table)
