18e5c185379dcb7dccc070045a3b1ff4973bdfab
angmorpri <angelmorenoprieto@gmail.com>
2024-07-04 20:07:36 +0200

    Test en "Factory" finalizados

--- marx/factory.py
+++ marx/factory.py
@@ -22,60 +22,90 @@ class Factory(Generic[FactoryItem]):
     GLOBAL_INDEX = 0
 
     def __init__(self, base: type[FactoryItem]) -> None:
-        super().__setattr__("_base", None)
-        super().__setattr__("_items", None)
+        super().__setattr__("_baseclass", None)
+        super().__setattr__("_all", None)
         super().__setattr__("_status", None)
-        super().__setattr__("_subset", None)
-        self._base = base
-        self._items = {}
+        super().__setattr__("_handled", None)
+        super().__setattr__("_inactive_are_visible", None)
+        super().__setattr__("_parent", None)
+        self._baseclass = base
+        self._all = {}
         self._status = {}  # 0: eliminado, 1: activo, 2: modificado
-        self._subset = []  # subconjuntos de objetos manejados por esta lista
+        self._handled = []  # IDs de los objetos manejados por esta lista
+        self._inactive_are_visible = False
+        self._parent = None  # referencia a la lista padre, si existe
 
     # Privados
 
     @property
-    def _active(self) -> list[int]:
-        """Devuelve los IDs de los objetos activos
-
-        Un objeto está activo si es manejado por esta lista, y no está marcado
-        para eliminación (status == 0)
-
-        """
-        return [id for id in self._items if self._status[id] > 0 and id in self._subset]
+    def _visible(self) -> list[int]:
+        """IDs de los objetos visibles manejados por esta lista"""
+        if self._inactive_are_visible:
+            return [id for id in self._handled]
+        else:
+            return [id for id in self._handled if self._status[id] > 0]
 
     def _append(self, item: FactoryItem) -> int:
         Factory.GLOBAL_INDEX += 1
         id = Factory.GLOBAL_INDEX
-        self._items[id] = item
+        self._all[id] = item
         self._status[id] = 1
-        self._subset.append(id)
+        self._handled.append(id)
         return id
 
-    def _create_subset(self, *ids: int) -> Factory[FactoryItem]:
+    def _create_subset(
+        self, ids: list[int], *, inactive_are_visible: bool | None = None
+    ) -> Factory[FactoryItem]:
         # copia todos los objetos y estados, pero sólo maneja los IDs dados
-        subset = Factory(self._base)
-        subset._items = self._items
+        subset = Factory(self._baseclass)
+        subset._all = self._all
         subset._status = self._status
-        subset._subset = ids
+        subset._handled = ids
+        if inactive_are_visible is None:
+            subset._inactive_are_visible = self._inactive_are_visible
+        else:
+            subset._inactive_are_visible = inactive_are_visible
+        subset._parent = self
         return subset
 
     # Públicos
+
+    @property
+    def all(self) -> Factory[FactoryItem]:
+        """Subconjunto con todos los objetos de la lista, activos o no,
+        manejados por esta lista o no
+
+        """
+        return self._create_subset(self._all.keys(), inactive_are_visible=True)
+
+    @property
+    def any(self) -> Factory[FactoryItem]:
+        """Subconjunto con todos los objetos de la lista, activos o no,
+        manejados por esta lista
+
+        """
+        return self._create_subset(self._handled, inactive_are_visible=True)
+
+    @property
+    def active(self) -> Factory[FactoryItem]:
+        """Subconjunto con todos los objetos visibles de la lista"""
+        return self._create_subset(self._handled, inactive_are_visible=False)
 
     def new(self, *args: Any, **kwargs: Any) -> Factory[FactoryItem]:
         """Crea un nuevo objeto industrializado, usando el constructor de su
         base y los argumentos dados
 
-        Devuelve un subconjunto sólo con el objeto creado
+        Devuelve un subconjunto sólo con el objeto creado.
 
         """
         try:
-            item = self._base(*args, **kwargs)
+            item = self._baseclass(*args, **kwargs)
         except TypeError:
             raise TypeError(
-                f"[Factory] No se puede crear un objeto de tipo '{self._base.__name__}' con los argumentos dados."
+                f"[Factory] No se puede crear un objeto de tipo '{self._baseclass.__name__}' con los argumentos dados."
             ) from None
         id = self._append(item)
-        return self._create_subset(id)
+        return self._create_subset([id])
 
     def register(self, item: FactoryItem) -> Factory[FactoryItem]:
         """Registra un objeto en la lista y lo industrializa
@@ -86,124 +116,101 @@ class Factory(Generic[FactoryItem]):
         Devuelve un subconjunto sólo con el objeto registrado.
 
         """
-        if not isinstance(item, self._base):
+        if not isinstance(item, self._baseclass):
             raise TypeError(
-                f"[Factory] El objeto debe ser de tipo '{self._base.__name__}', no '{type(item).__name__}'."
+                f"[Factory] El objeto debe ser de tipo '{self._baseclass.__name__}', no '{type(item).__name__}'."
             )
         id = self._append(item)
-        return self._create_subset(id)
+        return self._create_subset([id])
 
     def fallback(self, *args: Any, **kwargs: Any) -> Factory[FactoryItem]:
         """Crea o registra un nuevo objeto industrializado, y lo asigna a
-        esta lista, si esta vacía o no tiene objetos activos
+        esta lista, si esta vacía o no tiene objetos visibles
 
         Si sólo se incluye un argumento y es del tipo de la base, se registra
-        el objeto mediante 'regsiter'. De lo contrario, se crea un objeto nuevo
+        el objeto mediante 'register'. De lo contrario, se crea un objeto nuevo
         mediante 'new'.
 
         """
         if self.empty():
-            if not kwargs and len(args) == 1 and isinstance(args[0], self._base):
-                return self.register(args[0])
-            return self.new(*args, **kwargs)
+            if not kwargs and len(args) == 1 and isinstance(args[0], self._baseclass):
+                return self._parent.register(args[0])
+            return self._parent.new(*args, **kwargs)
 
     def select(self, *attrs: str) -> list[list[Any]]:
         """Devuelve una matriz con los valores de los atributos dados de los
-        objetos activos
+        objetos visibles
 
         Si un atributo no existe en un objeto, se asigna None en su lugar
 
         """
-        return [[getattr(self._items[id], attr, None) for attr in attrs] for id in self._active]
+        return [[getattr(self._all[id], attr, None) for attr in attrs] for id in self._visible]
 
     def __getattr__(self, attr: str) -> list[Any]:
         """Sugarcoat para 'select', pero devuelve una lista simple"""
         return [row[0] for row in self.select(attr)]
 
     def update(self, **kwargs: Any) -> None:
-        """Actualiza los atributos de todos los objetos activos según los
+        """Actualiza los atributos de todos los objetos visibles según los
         argumentos dados
 
         Si se usan argumentos que no existen, se ignoran.
 
         """
-        for id in self._active:
+        for id in self._visible:
             for attr, value in kwargs.items():
-                if hasattr(self._items[id], attr):
-                    setattr(self._items[id], attr, value)
+                if hasattr(self._all[id], attr):
+                    setattr(self._all[id], attr, value)
+                    self._status[id] = 2
 
     def __setattr__(self, attr: str, value: Any) -> None:
         """Sugarcoat para 'update'"""
         # 'attr' no debe existir en 'Factory'
-        if not hasattr(self, attr):
+        if attr not in self.__dict__:
             self.update(**{attr: value})
         else:
             super().__setattr__(attr, value)
 
     def delete(self) -> None:
-        """Marca todos los objetos activos para eliminación"""
-        for id in self._active:
+        """Marca todos los objetos visibles para eliminación"""
+        for id in self._visible:
             self._status[id] = 0
 
-    def __del__(self) -> None:
-        """Marca todos los objetos activos para eliminación"""
-        self.delete()
-
     def pull(self) -> list[FactoryItem]:
-        """Devuelve una lista con los objetos activos"""
-        return [self._items[id] for id in self._active]
+        """Devuelve una lista con los objetos visibles"""
+        return [self._all[id] for id in self._visible]
 
     def pullone(self) -> FactoryItem | None:
-        """Devuelve un sólo objeto, el primero de los que estén activos
+        """Devuelve un sólo objeto, el primero de los que estén visibles
 
-        Si no hay objetos activos, devuelve None
+        Si no hay objetos visibles, devuelve None
 
         """
         try:
-            return self._items[next(iter(self._active))]
+            return self._all[next(iter(self._visible))]
         except StopIteration:
             return None
 
     def __iter__(self) -> Iterator[Factory[FactoryItem]]:
-        """Itera sobre los objetos activos
+        """Itera sobre los objetos visibles
 
         Devuelve subconjuntos con un sólo objeto, nunca el objeto directamente
 
         """
-        yield from (self._create_subset(id) for id in self._active)
-
-    def iter_all(self) -> Iterator[Factory[FactoryItem]]:
-        """Itera sobre todos los objetos, activos y marcados para eliminación
-
-        Al igual que '__iter__', devuelve subconjuntos con un sólo objeto
-
-        """
-        for id in self._items:
-            yield self._create_subset(id)
+        yield from (self._create_subset([id]) for id in self._visible)
 
     def sort(self, attr: str, *attrs: str, reverse: bool = False) -> Factory[FactoryItem]:
-        """Genera un nuevo subconjunto con los objetos activos, ordenados por
+        """Genera un nuevo subconjunto con los objetos visibles, ordenados por
         los atributos dados
 
         """
         return self._create_subset(
-            *sorted(
-                self._active,
-                key=lambda id: [getattr(self._items[id], a) for a in (attr, *attrs)],
-                reverse=reverse,
-            )
-        )
-
-    def sort_all(self, attr: str, *attrs: str, reverse: bool = False) -> Factory[FactoryItem]:
-        """Genera un nuevo subconjunto con todos los objetos, ordenados por los
-        atributos dados
-
-        """
-        return self._create_subset(
-            *sorted(
-                self._items,
-                key=lambda id: [getattr(self._items[id], a) for a in (attr, *attrs)],
-                reverse=reverse,
+            list(
+                sorted(
+                    self._visible,
+                    key=lambda id: [getattr(self._all[id], a) for a in (attr, *attrs)],
+                    reverse=reverse,
+                )
             )
         )
 
@@ -228,18 +235,40 @@ class Factory(Generic[FactoryItem]):
             else:
                 funcs += (lambda item, a=attr, v=value: getattr(item, a) == v,)
         # filtrar
-        ids = [id for id in self._active if all(f(self._items[id]) for f in funcs)]
-        return self._create_subset(*ids)
+        ids = [id for id in self._visible if all(f(self._all[id]) for f in funcs)]
+        return self._create_subset(ids)
 
     def empty(self) -> bool:
-        """Devuelve True si no hay objetos activos"""
-        return not bool(self._active)
+        """Devuelve True si no hay objetos visibles"""
+        return not bool(self._visible)
 
     def __len__(self) -> int:
-        """Devuelve la cantidad de objetos activos"""
-        return len(self._active)
+        """Devuelve la cantidad de objetos visibles"""
+        return len(self._visible)
+
+    def __getitem__(self, key: int | slice) -> Factory[FactoryItem]:
+        """Devuelve un subconjunto con el objeto en la posición dada"""
+        if isinstance(key, int):
+            return self._create_subset([self._visible[key]])
+        elif isinstance(key, slice):
+            return self._create_subset(self._visible[key])
 
     # Representación
 
     def __repr__(self) -> str:
-        return f"Factory({self._base.__name__}, {len(self)} objetos activos)"
+        return f"Factory({self._baseclass.__name__}, {len(self)} objetos visibles)"
+
+    def __str__(self) -> str:
+        return f"Factory de objetos '{self._baseclass.__name__}'"
+
+    def dbg_show(self, title: str = "<GENERIC>") -> None:
+        print(title)
+        print(f"  Base: {self._baseclass.__name__}")
+        print(f"  Objetos visibles: {len(self)}")
+        print(f"  Objetos activos: {len(self.active)}")
+        print(f"  Objetos manejados: {len(self.any)}")
+        print(f"  Objetos totales: {len(self.all)}")
+        print(f"  Objetos:")
+        for id in self._visible:
+            print(f"    {id} | {self._status[id]} | {self._all[id]!r}")
+        print()
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -2,6 +2,7 @@
 # Creado: 02/08/2024
 """Test de la clase Factory."""
 import os
+from random import choice, randint
 import sys
 
 MARX_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__) + "/marx"))
@@ -23,27 +24,67 @@ class MyClass:
 def test_creation():
     factory = Factory(MyClass)
 
-    # new(), register(), __iter__(), pull(), pullone()
-    new = factory.new("First", 1)
-    print(factory)
-    myobj = MyClass("Second", 2)
+    # new(), register()
+    new = factory.new("Juan", 1)
+    new.dbg_show("NEW")
+    myobj = MyClass("Ana", 2)
     reg = factory.register(myobj)
-    print(factory)
-    for item in factory:
-        print(" - ", item.pull(), item.pullone(), item, type(item), sep="; ")
-    print()
-    for item in factory:
-        print(" - ", item.pull(), item.pullone(), item, type(item), sep="; ")
+    reg.dbg_show("REG")
+    factory.dbg_show("FACTORY AFTER NEW AND REGISTER")
+    factory.new("Pedro", 3)
+    factory.new("María", 4)
+    factory.new("José", 5)
+    factory.new("Carla", 6)
 
+    # __getitem__(), delete(), all, any, active
+    subset = factory[3:]
+    subset.dbg_show("SUBSET")
+    subset.delete()
+    subset.dbg_show("SUBSET AFTER DELETE")
+    subset.any.dbg_show("SUBSET AFTER DELETE WITH ANY")
+    factory.dbg_show("FACTORY AFTER DELETE")
+    factory.any.dbg_show("FACTORY AFTER DELETE WITH ANY")
+    factory.any.active.dbg_show("FACTORY ACTIVE AFTER DELETE WITH ANY AND THEN ACTIVE")
+
+    # select() y __getitem__()
+    print(factory.select("name", "value"))
+    print(factory.any.value)
     print()
 
-    # update(), __setattr__()
+    # update() y __setattr__()
     factory.update(value=10)
+    factory.dbg_show("FACTORY AFTER UPDATE")
+    factory.any.dbg_show("FACTORY AFTER UPDATE WITH ANY")
+    subset.all.value = 20
+    factory.dbg_show("FACTORY AFTER SUBSET.ALL IS UPDATED")
+
+    # pull() y pullone()
+    print(factory.pull())
+    print(factory.pullone())
+    print()
+
+    # sort()
+    x = factory.all.sort("name")
+    x.dbg_show("SORTED")
+    x.all.dbg_show("SORTED ALL")
+    x = subset.sort("name")
+    x.dbg_show("SORTED SUBSET")
+
+    # subset()
     for item in factory:
-        print(item.pull(), item.pullone(), item, type(item), sep="; ")
-    factory.value = 20
+        item.value = randint(1, 10)
+    x = factory.subset(lambda x: x.value >= 5)
+    x.dbg_show("SUBSET WITH LAMBDA FILTER")
     for item in factory:
-        print(item.pull(), item.pullone(), item, type(item), sep="; ")
+        item.name = choice(["Bob", "Alice"])
+    x = factory.subset(lambda x: x.value >= 5, name="Bob")
+    x.dbg_show("SUBSET WITH LAMBDA FILTER AND KEY FILTER")
+    x.all.dbg_show("SUBSET WITH LAMBDA FILTER AND KEY FILTER (ALL)")
+
+    # fallback()
+    fb = factory.subset(name="John").fallback(name="John", value=100)
+    fb.dbg_show("FALLBACK ITEM")
+    factory.dbg_show("FACTORY AFTER FALLBACK")
 
 
 if __name__ == "__main__":
